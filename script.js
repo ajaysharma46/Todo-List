14df0ab74a3c0ce87dfcd4382cf0e793ad43a60e
ajay sharma <foodlesscoderajay@gmail.com>
2026-04-28 23:24:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,106 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const storedTasks = JSON.parse(localStorage.getItem('tasks'));
+    if (storedTasks) {
+        tasks = storedTasks;
+        updateTasksList();
+        updateStats();
+    }
+});
+
+// what is JSON.parse
+
+let tasks = [];
+
+const saveTasks = () => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+// JSON.stringify
+const addTask = () => {
+    const taskInput = document.getElementById("taskInput");
+    const text = taskInput.value.trim();
+
+    if (text) {
+        tasks.push({ text: text, completed: false });
+        // text:text, completed: false
+        taskInput.value = "";
+        updateTasksList();
+        updateStats();
+        saveTasks();
+    }
+};
+
+const deleteTask = (index) => {
+    tasks.splice(index, 1);
+    updateTasksList();
+    updateStats();
+    saveTasks();
+};
+
+const editTask = (index) => {
+    const taskInput = document.getElementById("taskInput");
+    taskInput.value = tasks[index].text;
+    tasks.splice(index, 1);
+    updateTasksList();
+    updateStats();
+    saveTasks();
+};
+
+const updateStats = () => {
+    const completeTasks = tasks.filter(task => task.completed).length;
+    const totalTasks = tasks.length;
+    const progress = totalTasks ? (completeTasks / totalTasks) * 100 : 0;
+    const progressBar = document.getElementById('progress');
+
+    progressBar.style.width = `${progress}%`;
+    document.getElementById('numbers').innerText = `${completeTasks} / ${totalTasks}`;
+
+  
+    if (tasks.length && completeTasks === totalTasks){
+      const jsConfetti = new JSConfetti()
+
+jsConfetti.addConfetti()
+    }
+};
+
+const toggleTaskComplete = (index) => {
+    tasks[index].completed = !tasks[index].completed;
+    updateTasksList();
+    updateStats();
+    saveTasks();
+};
+
+const updateTasksList = () => {
+    const taskList = document.getElementById('task-list');
+    taskList.innerHTML = '';
+
+    tasks.forEach((task, index) => {
+        const listItem = document.createElement("li");
+
+        listItem.innerHTML = `
+        <div class="taskItem">
+            <div class="task ${task.completed ? 'completed' : ''}">
+                <input type="checkbox" class="checkbox" ${task.completed ? "checked" : ""}/>
+                <p>${task.text}</p>
+            </div>  
+            <div class="icons">
+                <img src="check.png" onClick="editTask(${index})" />
+                <img src="del.png" class="del" onClick="deleteTask(${index})" />
+            </div>
+        </div>
+        `;
+
+        listItem.querySelector('.checkbox').addEventListener('change', () => toggleTaskComplete(index));
+        taskList.append(listItem);
+    });
+};
+
+document.getElementById("newTask").addEventListener("click", function (e) {
+    e.preventDefault();
+    addTask();
+});
+
+
+
+
+
+
